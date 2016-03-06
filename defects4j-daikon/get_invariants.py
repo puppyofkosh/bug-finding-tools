21f22c42c96ca3f69149d423352ead20db5002eb
ian boros <puppyofkosh@gmail.com>
2016-03-06 17:56:26 -0500

    More work on invariant generating script

--- defects4j-daikon/get_invariants.py
+++ defects4j-daikon/get_invariants.py
@@ -8,9 +8,32 @@ PASS_ARG = "pass"
 FAIL_ARG = "both"
 
 # Don't generate invaraints about stuff we don't care about
-CLASSES_TO_OMIT = ["^org.junit\.",
-                   "^junit\.",
-                   "^com.sun.proxy\."]
+CLASSES_TO_OMIT = ["^org.junit\\.",
+                   "^junit\\.",
+                   "^com.sun.proxy\\."]
+
+
+PROJECTS = {
+    "lang": {
+        "classpath": "target/tests:target/classes:" +
+        "$CLASSPATH:/usr/share/java/hamcrest-core.jar",
+        "classes_to_include": []
+    },
+
+    "closure": {
+        "classpath": "build/test:build/classes:build/lib/*:lib/*:$CLASSPATH",
+        "classes_to_include": ["__Invariant$", ]
+    }
+
+}
+
+LANG_CLASSPATH = "target/tests:target/classes:$CLASSPATH:" +\
+                 "/usr/share/java/hamcrest-core.jar"
+
+
+CLOSURE_CLASSPATH = "build/test:build/classes:build/lib/*:lib/*:$CLASSPATH"
+CLOSURE_CLASSES_TO_INCLUDE = ["__Invariant$", ]
+
 
 def get_omit_args(to_omit):
     s = ""
@@ -18,27 +41,48 @@ def get_omit_args(to_omit):
         s += "--ppt-omit-pattern='{0}' ".format(o)
     return s
 
+
+def get_include_args(to_include):
+    s = ""
+    for o in to_include:
+        s += "--ppt-select-pattern='{0}' ".format(o)
+    return s
+
+
+def get_project(projectname):
+    name = projectname.split("_")[0].lower()
+
+    if name not in PROJECTS:
+        print "{0} doesn't start with one of {1}".format(name,
+                                                         PROJECTS.keys())
+        raise RuntimeError("Unkown project")
+
+    return PROJECTS[name]
+
+
 def main():
     if len(sys.argv) < 3:
-        print "usage: {0} directoryname fullclasspathtorun [nodaikon]".format(sys.argv[0])
-        print "[nodaikon]: add to command if you just want to run the class without daikon"
+        print "usage: {0} directoryname fullclasspathtorun [nodaikon]"\
+            .format(sys.argv[0])
+        print "[nodaikon]: if you just want to run the class without daikon"
         return
 
-    project = sys.argv[1]
+    project_name = sys.argv[1]
     full_classname = sys.argv[2]
     file_name = full_classname.split('.')[-1]
     nodaikon = "nodaikon" in sys.argv
 
-    os.chdir(project)
+    project_obj = get_project(project_name)
+
+    os.chdir(project_name)
     return_code = call("defects4j compile", shell=True)
     if return_code != 0:
         print "couldn't compile"
         return
 
-    classpath = "target/tests:target/classes:$CLASSPATH:/usr/share/java/hamcrest-core.jar"
-
     if nodaikon:
-        command = "java -cp {0} {1} {2}".format(classpath, full_classname, FAIL_ARG)
+        command = "java -cp {0} {1} {2}".format(project_obj["classpath"],
+                                                full_classname, FAIL_ARG)
         call(command, shell=True)
         return
 
@@ -47,11 +91,15 @@ def main():
     # Omit invariants about junit internal stuff,
     # As well as invariants about the test cases (we want invariants about the
     # actual program)
-    chicory_args = get_omit_args(CLASSES_TO_OMIT + [full_classname])
+    to_omit = CLASSES_TO_OMIT + [full_classname]
+    to_include = project_obj["classes_to_include"]
+    chicory_args = get_omit_args(to_omit) + " " + get_include_args(to_include)
     command = "java -cp {0} daikon.Chicory {1} --daikon-args='{2}' --daikon {3}"\
-        .format(classpath, chicory_args, daikon_args, full_classname)
-    
-    print_invariants = "java daikon.PrintInvariants --format java {0}.inv.gz".format(file_name)
+        .format(project_obj["classpath"],
+                chicory_args, daikon_args, full_classname)
+
+    print_invariants = "java daikon.PrintInvariants --format java {0}.inv.gz".\
+                       format(file_name)
 
     if "failonly" not in sys.argv:
         print command
@@ -65,8 +113,8 @@ def main():
         if 0 != call(command + " " + FAIL_ARG, shell=True):
             print "Failing version crashed"
             return
-        call(print_invariants + " > fail_invariants",shell=True)
-    
+        call(print_invariants + " > fail_invariants", shell=True)
+
 
 if __name__ == "__main__":
     main()
